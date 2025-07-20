a65938ff949072c0be037a903dde81d5be034d11
Gabriel Korherr <gabrielkorherr@gmail.com>
2025-07-20 20:47:01 +0200

    Scrapped

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,3 @@
-use goo::Game;
-use goo::script::Interpreter;
-
 fn main() {
-    let mut game = Game::new(String::from("exmpl"));
-    game.play();
 
-    Interpreter::new().execute(String::from("[] {\n\tglob helloo = 69\n\tout helloo\n}"));
 }
